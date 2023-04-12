83a088a22fac1a101822482aecbfd5d1d80966d1
stevescilar <stevemuambi@gmail.com>
2023-04-12 15:01:32 +0300

    some updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,11 @@ createInertiaApp({
   resolve: async (name) => {
     const pages = import.meta.glob('./Pages/**/*.vue')
 
+    // specific component view
     return (await pages[`./Pages/${name}.vue`]())
 
   },
+  // create  mount and run vue pages
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
