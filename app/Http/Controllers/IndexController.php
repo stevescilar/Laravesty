e51a73e94d543d2e908750c35efea6471bfdeeae
stevescilar <stevemuambi@gmail.com>
2023-04-13 23:40:32 +0300

    updated vue page(index)

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,12 @@ class IndexController extends Controller
     //function goes here
      public function index()
      {
-       return inertia('Index/Index');
+       return inertia('Index/Index',
+      
+       [
+        'message' => 'Hello from the other side!'
+       ]
+      );
      }
 
      public function show() 
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,9 +1,14 @@
 <template>
     <div>Index</div>
     <Link href="/show">Show Page</Link>
+    <div>
+        The message is  {{ message }}
+    </div>
 </template>
 
 <!-- logic section -->
 <script setup>
-    import { Link } from '@inertiajs/vue3'
+import { Link } from '@inertiajs/vue3'
+// pass message to vuepage
+defineProps(['message'])
 </script>
