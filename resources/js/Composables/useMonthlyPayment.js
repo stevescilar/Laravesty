d533114b5e3900bc4633e83aaf7eaf83c63cdff2
stevescilar <stevemuambi@gmail.com>
2023-04-21 00:16:40 +0300

    updates on show.vue

--- resources/js/Composables/useMonthlyPayment.js
+++ resources/js/Composables/useMonthlyPayment.js
@@ -2,22 +2,22 @@
 import { computed, isRef  } from 'vue';
 
 export const useMonthlyPayment = (total, interestRate, duration) => {
-    const monthlyPayment = computed(() => {
+  const monthlyPayment = computed(() => {
 
-        const principal = isRef(total) ? total.value : total
-        const monthlyInterest = (isRef(interestRate) ? interestRate.value : interestRate) / 100 / 12
-        const numberOfPaymentMonths = (isRef(duration) ? duration.value : duration) * 12
+    const principal = isRef(total) ? total.value : total
+    const monthlyInterest = (isRef(interestRate) ? interestRate.value : interestRate) / 100 / 12
+    const numberOfPaymentMonths = (isRef(duration) ? duration.value : duration) * 12
 
-        return principal * monthlyInterest * (Math.pow(1 + monthlyInterest,
-             numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, 
-                numberOfPaymentMonths) - 1)
-    })
+    return principal * monthlyInterest * (Math.pow(1 + monthlyInterest,
+      numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, 
+      numberOfPaymentMonths) - 1)
+  })
 
-    const totalPaid = computed (() => {
-        return (isRef(duration) ? duration.value : duration) * 12 * monthlyPayment.value
-    })
+  const totalPaid = computed (() => {
+    return (isRef(duration) ? duration.value : duration) * 12 * monthlyPayment.value
+  })
 
-    const totalInterest = computed(() => totalPaid.value - (isRef(total) ? total.value : total))
+  const totalInterest = computed(() => totalPaid.value - (isRef(total) ? total.value : total))
     
-    return { monthlyPayment, totalPaid, totalInterest }
+  return { monthlyPayment, totalPaid, totalInterest }
 }
