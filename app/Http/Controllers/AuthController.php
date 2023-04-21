5cbfdafb38067156fa6d26a14609639a0df22eba
stevescilar <stevemuambi@gmail.com>
2023-04-21 15:19:41 +0300

    SIgnin Logic authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 // user session
 class AuthController extends Controller
@@ -12,8 +14,21 @@ class AuthController extends Controller
         return inertia('Auth/Login');
     }
 
-    public function store() {
+    public function store(Request $request) {
         // save user
+        if (!Auth::attempt($request->validate([
+            'email' => 'required|string|email',
+            'password' =>'required|string'
+        ]), true)){
+            throw ValidationException::withMessages([
+                'email' => 'Authentication Failed'
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended();
+
     }
 
     public function destroy(){
