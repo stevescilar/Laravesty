44b3981650d9e4df31b9fc9cdfcbe9edd7163579
stevescilar <stevemuambi@gmail.com>
2023-04-22 19:18:32 +0300

    working on some bug-auth

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 use App\Http\Controllers\AuthController;
 
 
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/show',[IndexController::class, 'show']);
+Route::get('/show',[IndexController::class, 'show'])->middleware('auth');
 
 // Route::resource('listing', ListingController::class)->only(['index', 'show', 'create','store']);
-Route::resource('listing', ListingController::class);
-
+Route::resource('listing', ListingController::class)->only(['create','store', 'edit', 'update', 'destroy'])->middleware('auth');
+Route::resource('listing', ListingController::class)->except(['create','store', 'edit', 'update', 'destroy']);
 // Login routes
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
