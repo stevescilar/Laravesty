afa7d3c9f883450bb5b893c8f8a3a6811b7b4586
stevescilar <stevemuambi@gmail.com>
2023-04-26 15:45:18 +0300

    updated

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,8 @@
 <template>
-  <!-- <Link href="/listing/create">New Listing</Link> -->
-
   <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
-          <!-- <Link :href="{{ route('listing.index') }}">C</Link> -->
           <Link :href="route('listing.index')">Listings</Link>
         </div>
         <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
